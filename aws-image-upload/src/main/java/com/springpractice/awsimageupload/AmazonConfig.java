28b649c1a6984b5c863f36482d5fc913b2c07bb4
John Bigornia <32423793+johnbigornia@users.noreply.github.com>
2021-10-21 14:39:00 -1000

    init

---
+++ aws-image-upload/src/main/java/com/springpractice/awsimageupload/AmazonConfig.java
@@ -0,0 +1,24 @@
+package com.springpractice.awsimageupload;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AmazonConfig {
+
+    @Bean
+    public AmazonS3 s3() {
+        AWSCredentials awsCredentials = new BasicAWSCredentials("AKIA2F3WSDTQPVI2CJ7P", "mqwQEKdwNYzxz9NqYsfw+9bYmAjQSWfnUAhPoehw");
+
+        return AmazonS3ClientBuilder
+                .standard()
+                .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
+                .build();
+    }
+}
